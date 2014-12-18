330b28c7a7fde5dd07b9adc7150a947e2df8e75f
Tamas Gal <himself@tamasgal.com>
2014-12-18 10:55:34 +0100

    Adds doctests for get_version

--- controlhost/__version__.py
+++ controlhost/__version__.py
@@ -5,13 +5,18 @@
 Pep 386 compliant version info.
 
     (major, minor, micro, alpha/beta/rc/final, #)
-    (1, 1, 2, 'alpha', 0) => "1.1.2.dev"
-    (1, 2, 0, 'beta', 2) => "1.2b2"
+
+Example:
+
+    >>> get_version((1, 1, 2, 'alpha', 0))
+    '1.1.2.dev'
+    >>> get_version((1, 2, 0, 'beta', 2))
+    '1.2b2'
 
 """
 version_info = (0, 1, 0, 'final', 0)
 
-def _get_version(version_info):
+def get_version(version_info):
     """Return a PEP 386-compliant version number."""
     assert len(version_info) == 5
     assert version_info[3] in ('alpha', 'beta', 'rc', 'final')
@@ -28,4 +33,5 @@ def _get_version(version_info):
 
     return str(main + sub)
 
-version = _get_version(version_info)
+version = get_version(version_info)
+

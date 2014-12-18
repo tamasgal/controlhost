2e2a85a73971fc8227c03ce6cc9f8819d2618fe9
Tamas Gal <himself@tamasgal.com>
2014-12-18 11:44:08 +0100

    Adds module docstring

--- controlhost/tests/test_controlhost.py
+++ controlhost/tests/test_controlhost.py
@@ -2,6 +2,7 @@
 # Filename: test_controlhost.py
 # pylint: disable=locally-disabled,C0111,R0904,R0201,C0103,W0612
 """
+Unit tests for the controlhost module.
 
 """
 from __future__ import absolute_import, print_function

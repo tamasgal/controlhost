307f72a353c210fdf2ebde0e8a9a6f2c31c0cda9
Tamas Gal <tgal@km3net.de>
2015-05-05 19:03:52 +0200

    Client usage example

--- examples/client.py
+++ examples/client.py
@@ -3,5 +3,12 @@ from controlhost import Client
 client = Client('localhost')
 client._connect()
 client.subscribe('foo')
-print client.get_message()
-client._disconnect()
+
+try:
+    while True:
+        prefix, message = client.get_message()
+        print prefix.tag
+        print prefix.length
+        print message
+except KeyboardInterrupt:
+    client._disconnect()

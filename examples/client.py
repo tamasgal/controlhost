959f54780f1242cb1f86d6dfec1e102be96cc989
Tamas Gal <himself@tamasgal.com>
2015-10-01 13:31:24 +0200

    Uses the with context

--- examples/client.py
+++ examples/client.py
@@ -1,14 +1,12 @@
 from controlhost import Client
 
-client = Client('localhost')
-client._connect()
-client.subscribe('foo')
-
-try:
-    while True:
-        prefix, message = client.get_message()
-        print prefix.tag
-        print prefix.length
-        print message
-except KeyboardInterrupt:
-    client._disconnect()
+with Client('131.188.161.241') as client:
+    client.subscribe('foo')
+    try:
+        while True:
+            prefix, message = client.get_message()
+            print prefix.tag
+            print prefix.length
+            print message
+    except KeyboardInterrupt:
+        client._disconnect()

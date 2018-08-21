35970bd7849d2fecc35c3825f3b1f31cca6f50a4
Tamas Gal <tgal@km3net.de>
2018-08-21 17:10:21 +0200

    Restructuring and extracting controlhost from km3pipe

--- controlhost/tests/test_controlhost.py
+++ controlhost/tests/test_controlhost.py
@@ -1,27 +1,29 @@
-# coding=utf-8
 # Filename: test_controlhost.py
 # pylint: disable=locally-disabled,C0111,R0904,R0201,C0103,W0612
 """
 Unit tests for the controlhost module.
 
 """
-from __future__ import absolute_import, print_function
-
 import unittest
 
 from controlhost import Tag, Message, Prefix
 
+__author__ = "Tamas Gal"
+__copyright__ = "Copyright 2018, Tamas Gal and the KM3NeT collaboration."
+__credits__ = []
+__license__ = "MIT"
+__maintainer__ = "Tamas Gal"
+__email__ = "tgal@km3net.de"
+__status__ = "Development"
+
 
 class TestTag(unittest.TestCase):
-    def test_init(self):
-        tag = Tag()
-
     def test_empty_tag_has_correct_length(self):
         tag = Tag()
         self.assertEqual(Tag.SIZE, len(tag))
 
     def test_tag_has_correct_length(self):
-        for tag_name in ('foo', 'bar', 'baz', '1'):
+        for tag_name in (b'foo', b'bar', b'baz', b'1'):
             tag = Tag(tag_name)
             self.assertEqual(Tag.SIZE, len(tag))
 
@@ -29,22 +31,21 @@ class TestTag(unittest.TestCase):
         self.assertRaises(ValueError, Tag, '123456789')
 
     def test_tag_has_correct_data(self):
-        tag = Tag('foo')
-        self.assertEqual('foo\x00\x00\x00\x00\x00', tag.data)
+        tag = Tag(b'foo')
+        self.assertEqual(b'foo\x00\x00\x00\x00\x00', tag.data)
         tag = Tag('abcdefgh')
         self.assertEqual('abcdefgh', tag.data)
 
     def test_tag_has_correct_string_representation(self):
-        tag = Tag('foo')
+        tag = Tag(b'foo')
         self.assertEqual('foo', str(tag))
-
 
 
 class TestPrefix(unittest.TestCase):
     def test_init(self):
-        prefix = Prefix('foo', 1)
+        Prefix(b'foo', 1)
 
 
 class TestMessage(unittest.TestCase):
     def test_init(self):
-        message = Message('')
+        Message('')
